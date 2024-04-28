74d29f835b92bfe5e9613271c0389decbfd0d6c1
MartinWinterSC <Martinsc@live.dk>
2024-04-29 00:49:23 +0200

    Complete design of Front page
    Header, Footer and FEP have a unified CSS document now
    The rest have been seperated into a Frontpage CSS document

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-const baseURL = "https://module5api.martinwinther.dk"
+// const baseURL = "https://module5api.martinwinther.dk"
 
-fetch(baseURL + `=${}`)
-.then((res) => res.json())
-.then()
-.catch(err => console.log("Something went wrong, try again later"))
+// fetch(baseURL + `=${}`)
+// .then((res) => res.json())
+// .then()
+// .catch(err => console.log("Something went wrong, try again later"))
