1dc2b002bab16781e7c380f1010c20b21a938117
MartinWinterSC <Martinsc@live.dk>
2024-04-30 00:02:20 +0200

    additions
    added
     - icons
     - recipeOverview.html
     - recipeOverview.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
-// const baseURL = "https://module5api.martinwinther.dk"
+const favContainerEl = document.querySelector(".recommendationsContainer");
 
-// fetch(baseURL + `=${}`)
-// .then((res) => res.json())
-// .then()
-// .catch(err => console.log("Something went wrong, try again later"))
+const baseURL = "https://foodapi.starsingh.dk/wp-json/wp/v2/posts";
+
+fetch(baseURL)
+  .then((res) => res.json())
+  .then((posts) => {
+    posts.forEach((post) => {
+      console.log(post);
+
+      const articleHTML = `
+        <article>
+          <div class="articleImg"></div>
+          <div class="cardInfo" id="${post.id}">
+            <h3>${post.acf.title}</h3>
+            <div>
+              <img src="./assets/img/icons/clock-regular.svg" alt="" class="infoIcon">
+              <p></p>
+              <img src="" alt="" class="infoIcon">
+            </div>
+          </div>
+        </article>`;
+
+      favContainerEl.innerHTML += articleHTML;
+    });
+  })
+  .catch((err) => {
+    console.log("Something went wrong, try again later", err);
+  });
