efb17d431d34a6642624ef324fe0f46fafb6e7d4
MartinWinterSC <Martinsc@live.dk>
2024-05-02 04:09:50 +0200

    Finalized V1

--- assets/js/fepScript.js
+++ assets/js/fepScript.js
@@ -1,2 +1,32 @@
+document.addEventListener('DOMContentLoaded', async function () {
+    const fepContainerEL = document.querySelector(".fepContainer");
 
-fetch
+    const baseURL = "https://foodapi.starsingh.dk/wp-json/wp/v2/posts";
+    const urlCategoryRecipe = "?categories=3&per_page=100";
+
+    try {
+        const response = await fetch(baseURL + urlCategoryRecipe);
+        const posts = await response.json();
+        
+        console.log(posts);
+
+        const shuffledPosts = posts.sort(() => 0.5 - Math.random());
+        const selectedPosts = shuffledPosts.slice(0, 3);
+
+        fepContainerEL.innerHTML = '';
+
+        selectedPosts.forEach((post) => {
+            const renderFepRecipe = `
+                <article class="fepSingle" style="background: url('${post.acf.image.link}') center no-repeat;">
+                    <a href="./Opskriften.html?id=${post.id}">
+                        <div class="overlay">
+                            <h5>${post.acf.title}</h5>
+                        </div>
+                    </a>
+                </article>`;
+            fepContainerEL.innerHTML += renderFepRecipe;
+        });
+    } catch (err) {
+        console.error("Something went wrong, try again later", err);
+    }
+});
